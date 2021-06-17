ed2cbf6318b18e895e8ef3eb5fa50f40fae7e3fa
Erdem <munkueveg@gmail.com>
2021-06-18 02:46:26 +0800

    review

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from .models import Post
+
+from .models import Group, Post
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("pk", "text", "pub_date", "author")
-    search_fields = ("text",)
-    list_filter = ("pub_date",)
-    empty_value_display = "-пусто-"
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+    #prepopulated_fields = {'slug': ('title',)}
 
 
 admin.site.register(Post, PostAdmin)
-# Register your models here.
+admin.site.register(Group) 
+
