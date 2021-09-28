dc5e27600765433e84ffda0239443ef0d1db7288
Erdem <munkueveg@gmail.com>
2021-09-28 08:21:11 +0800

    Делаю страницу пользователя с постами

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,15 +1,18 @@
+from django.contrib.auth import get_user_model
+from django.core import paginator
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
-from .models import Post, Group
+from .models import Post, Group, User
+
+
+User = get_user_model()
 
 
 def index(request):
     post_list = Post.objects.all().order_by('-pub_date')
     paginator = Paginator(post_list, 10)
-
     page_number = request.GET.get('page')
-
     page_obj = paginator.get_page(page_number)
     context = {
         'page_obj': page_obj,
@@ -21,9 +24,7 @@ def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.all()[:11]
     paginator = Paginator(posts, 10)
-
     page_number = request.GET.get('page')
-
     page_obj = paginator.get_page(page_number)
     context = {
         'page_obj': page_obj,
@@ -37,12 +38,15 @@ def group_posts(request, slug):
 
 
 def profile(request, username):
+    user = User.objects.get(username=username)
+    posts = Post.objects.filter(author=username)
+    paginator = Paginator(posts, 10)
+    page_number = request.Get.get('page')
+    page_obj = paginator.get_page(page_number)
+    count = Post.objects.filter(author=user).count()
     context = {
+        'page_obj': page_obj,
+        'author': user,
+        'count': count,
     }
     return render(request, 'posts/profile.html', context)
-
-
-def post_detail(request, post_id):
-    context = {
-    }
-    return render(request, 'posts/post_detail.html', context) 
